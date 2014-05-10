46f923a66596d79b1997155cb7442244b18414c2
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2014-05-11 00:33:59 +1000

    Removed dependency on non-standard json module. Now automatically converting Buffer objects to base64 objects

--- formatter.js
+++ formatter.js
@@ -1,18 +1,49 @@
-var json = require('json');
-var cycle = require('json/cycle');
+module.exports.parse = function (buffer) {
+	return JSON.parse(buffer.toString());
+};
 
-module.exports.parse = function (buffer, skipRetrocycle) {
-	var firstParse = json.parse(buffer.toString());
-	var secondParse;
-	var i;
-	if (skipRetrocycle) {
-		secondParse = firstParse;
-	} else {
-		secondParse = cycle.retrocycle(firstParse);
+var isOwnDescendant = function (object, ancestors) {
+	for (var i in ancestors) {
+		if (ancestors[i] === object) {
+			return true;
+		}
 	}
-	return secondParse;
+	return false;
+};
+
+var convertBuffersToBase64 = function (object, ancestors) {
+	if (!ancestors) {
+		ancestors = [];
+	}
+	if (isOwnDescendant(object, ancestors)) {
+		throw new Error('Cannot traverse circular structure');
+	}
+	var newAncestors = ancestors.concat([object]);
+	
+	if (object instanceof Buffer) {
+		return {
+			base64: true,
+			data: object.toString('base64')
+		};
+	}
+	if (object instanceof Array) {
+		var base64Array = [];
+		for (var i in object) {
+			base64Array[i] = convertBuffersToBase64(object[i], newAncestors);
+		}
+		return base64Array;
+	}
+	if (object instanceof Object) {
+		var base64Object = {};
+		for (var j in object) {
+			base64Object[j] = convertBuffersToBase64(object[j], newAncestors);
+		}
+		return base64Object;
+	}
+	return object;
 };
 
 module.exports.stringify = function (object) {
-	return json.stringify(cycle.decycle(object));
+	var base64Object = convertBuffersToBase64(object);
+	return JSON.stringify(base64Object);
 };
